507a0e9eb648fa3519849a26fbd99712efef4ead
Kandyyy <kingkandy016@gmail.com>
2022-03-29 11:30:37 +0530

    deffo didn't leak my password last commit

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'PenguinSor987@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Penguin_Sor123'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
